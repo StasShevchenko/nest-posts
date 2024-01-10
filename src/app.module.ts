b1083e10ec7232c860343f179e1b343366939b6f
Stas <dark.knight22832@gmail.com>
2024-01-10 15:04:45 +0300

    Simple auth functionality is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import {PrismaModule} from "./modules/prisma/prisma.module";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule, JwtService} from "@nestjs/jwt";
+import {AuthModule} from "./modules/auth/auth.module";
+import {UserModule} from "./modules/user/user.module";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      PrismaModule,
+      ConfigModule.forRoot(),
+      AuthModule,
+      UserModule,
+  ],
+    providers: [
+        JwtService,
+    ]
 })
 export class AppModule {}
